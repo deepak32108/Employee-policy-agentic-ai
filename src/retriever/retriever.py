564114a302a737795962c5522e03de438080045f
deepak32108 <penumakadeepak7@gmail.com>
2026-06-15 20:05:26 +0530

    Fix API URL for Render

--- src/retriever/retriever.py
+++ src/retriever/retriever.py
@@ -1,47 +1,25 @@
 from langchain_chroma import Chroma
-
 from src.embeddings.embedding_model import get_embeddings
 
 VECTOR_DB_PATH = "vectorstore"
 
+_embeddings = get_embeddings()
+
+_vector_db = Chroma(
+    persist_directory=VECTOR_DB_PATH,
+    embedding_function=_embeddings
+)
+
+_retriever = _vector_db.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 2}
+)
+
 
 def get_retriever():
-
-    embeddings = get_embeddings()
-
-    vector_db = Chroma(
-        persist_directory=VECTOR_DB_PATH,
-        embedding_function=embeddings
-    )
-
-    retriever = vector_db.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": 2}
-    )
-
-    return retriever
+    return _retriever
 
 
 def retrieve_documents(question: str):
-
-    retriever = get_retriever()
-
-    docs = retriever.invoke(question)
-
+    docs = _retriever.invoke(question)
     return docs
-
-
-if __name__ == "__main__":
-
-    query = input("Ask Question: ")
-
-    docs = retrieve_documents(query)
-
-    print("\nRetrieved Documents:\n")
-
-    for i, doc in enumerate(docs, start=1):
-
-        print(f"\n----- Document {i} -----")
-        print("Source:", doc.metadata.get("source"))
-        print("Page:", doc.metadata.get("page"))
-        print(doc.page_content[:500])
